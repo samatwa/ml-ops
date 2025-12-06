f6dc35ae0b1e2bc574f790c9e8da3b6f9fe75e74
ViacheslavKuznetsov <155986232+samatwa@users.noreply.github.com>
2025-12-06 09:51:17 +0100

    changes

--- argocd/main.tf
+++ argocd/main.tf
@@ -17,3 +17,65 @@ resource "helm_release" "argo" {
   
   values = [file("${path.module}/values/argocd-values.yaml")]
 }
+
+data "kubernetes_custom_resource_definition" "appset_crd" {
+  metadata {
+    name = "applicationsets.argoproj.io"
+  }
+  depends_on = [helm_release.argo]
+}
+
+resource "kubernetes_manifest" "namespaces_appset" {
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "ApplicationSet"
+
+    metadata = {
+      name      = "namespaces-appset"
+      namespace = var.argocd_namespace
+    }
+
+    spec = {
+
+      generators = [{
+        git = {
+          repoURL     = var.app_repo_url
+          revision    = var.app_repo_branch
+          directories = [{ path = "namespace/*" }]
+        }
+      }]
+
+      template = {
+        metadata = {
+          name      = "ns-{{path.basename}}"
+          namespace = var.argocd_namespace
+        }
+        spec = {
+          project = "default"
+
+          source = {
+            repoURL        = var.app_repo_url
+            targetRevision = var.app_repo_branch
+            path           = "{{path}}"
+            directory      = { recurse = true } # читати підкаталоги (apps/, configmaps/, secrets/)
+          }
+
+          destination = {
+            server    = "https://kubernetes.default.svc"
+            namespace = "{{path.basename}}" # application / infra-tools / ...
+          }
+
+          syncPolicy = {
+            automated   = { prune = true, selfHeal = true }
+            syncOptions = ["CreateNamespace=true"]
+          }
+          revisionHistoryLimit = 2
+        }
+      }
+    }
+  }
+  
+  depends_on = [data.kubernetes_custom_resource_definition.appset_crd]
+}
+
+
